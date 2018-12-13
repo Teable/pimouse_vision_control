2a081143bf8e2d5219480da7aa3bfad9656da274
Teable <blukey842unluck@gmail.com>
2018-12-13 15:14:51 +0000

    Update travis_package_make,bash

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,10 +1,21 @@
 #!/bin/bash -xve
 
-#sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+sudo apt-get update
+sudo apt-get install libopencv-dev python-opencv
 
+#sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+#sudo ldconfig
+
+#sudo apt-get install libopencv-dev
+sudo apt-get install ros-indigo-cv-bridge
+sudo apt-get install ros-indigo-cv-camera
+
+#sync and make
+rsync -av ./ ~/catkin_ws/src/cpimouse_vision_control/
+
+#clone rasraspimouse_ros_2_2
 cd ~/catkin_ws/src/
-git clone --depth=1 https://github.com/Teable/pimouse_ros.git
+git clone https://github.com/citueda/rasraspimouse_ros_2_2.git
 
 cd ~/catkin_ws
 catkin_make
